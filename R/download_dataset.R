9ee33ef658e56413d66fac0c9cdf0591726a8980
Jon Brock <jon@frankl.io>
2020-03-04 15:57:52 +1100

    Added meta to download_dataset

--- R/download_dataset.R
+++ R/download_dataset.R
@@ -9,16 +9,27 @@
 #' @return Returns a list
 #'
 #' @examples
-#' df <- download_dataset ("SPPeJC1VInG5VMXN8M6D")
+#' myData <- download_dataset ("SPPeJC1VInG5VMXN8M6D")
+#' 
+#' @importFrom magrittr %>%
+#' @importFrom jsonlite fromJSON
 #' 
 #' @export
+#' 
 download_dataset <- function(DatasetID) {
-  myDataObject <- download_data_object(DatasetID)
+  myDataObject <- make_post_request(Endpoint = "/user/downloadFile", Body = paste0('"dataSetId": "', DatasetID, '"')) %>% 
+    content(., "text") %>% 
+    jsonlite::fromJSON()
   
   myDataframe <- as.data.frame(myDataObject$data, stringsAsFactors = FALSE, row.names = FALSE)
   colnames(myDataframe) <- myDataObject$header$name
-  df <- transform_dataframe_columns_to_numeric(myDataframe)
+  myDataframe <- transform_dataframe_columns_to_numeric(myDataframe)
   
-  myList <- list("df"=df, "names"=myDataObject$header$name, "description"=myDataObject$header$description, "datasetId"=DatasetID)
+  myMeta <- list("datasetId"=DatasetID,
+                 "name"=myDataObject$header$name, 
+                 "description"=myDataObject$header$description, 
+                 "type"=myDataObject$header$type)
+  
+  myList <- list("df"=myDataframe, "meta"=myMeta)
 }
 
