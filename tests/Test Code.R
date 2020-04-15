47118a3b46058e21d27b341b80b143d23afd94ba
Jon Brock <jon@frankl.io>
2020-04-15 16:04:18 +1000

    updated test code

--- tests/Test Code.R
+++ tests/Test Code.R
@@ -1,38 +1,33 @@
-# Install the latest version of the hutr package (unless you know it hasn't changed)
+# Install the latest version of the hutr package
 library(devtools)
 install_github("hut34/hutr-package")
 
 # Load the hutr package
 library(hutr)
 
-# View datasets available on the hut
+# Initiate your hut session
 initiate_hut()
 
 #browse the datasets available in the hut
 browse_available_datasets()
 
 #upload a data frame to the hut as a dataset - simple example using car dataset built in to R
-upload_dataset(mtcars, Name="mtcars", Description="Data about cars")
+upload_dataset(mtcars, Name="mtcars", Description="Some data about cars")
+
+#look for it in your datasets
+view_my_datasets()
 
 #browse the datasets available in the hut
-View(view_all_datasets ())
+browse_available_datasets ()
 
-# Download a dataset that I uploaded (only works for datasets uploaded by the user, because of the key management)
-myData <- download_uploaded_dataset (DatasetNumber=4)
-View(myData)
+#choose one to acquire - this will require you to replace the ID with the ID from the table.
+acquire_dataset("kbkjbAjbSOjb0kjbu37")
 
-#Acquire dataset from someone else
-View(view_all_datasets ())
-purchase_dataset (DatasetNumber=1)
+#check to see if it is now in your datasets - if not then the dataset owner is required to authorise transaction
+view_my_datasets()
 
-#Download previously acquired dataset
-View(view_purchased_datasets ())
-myPurchasedData <- download_purchased_dataset (DatasetNumber=1)
-View(myPurchasedData)
+# Download that dataset
+myData <- download_dataset ("kbkjbAjbSOjb0kjbu37")
 
-#complete some work here in R - simple example filtering the cars dataset to cars with less than 5 cylinders
-myNewData <- filter(myData, cyl < 5)
-
-#upload back to the hut - enter the appropriate dataFrame name, and meta details below (talk to us to lean more about 'Parent')
-upload_dataset(myNewData, Name="A new dataset", Description="A dataset created in R and pushed to the hut", Parent="parentID")
-#confirm that the dataset uploaded by repeating line 10 / 11 above!
+# View dataframe
+View(myData$df)
